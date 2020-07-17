23591e291a8e971d04be3857dc6cf9bd598940f6
Danze <hoangvpse130555@fpt.edu.vn>
2020-07-17 22:47:47 +0700

    Remove IHostingEnvironment

--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
@@ -112,7 +113,7 @@ namespace GazeManager
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseErrorHandler();
 
